8e96f9cc8f2dc9c61d17918b0ad7d1ff777099e0
CaptainTheAbysswalker <shuric03@gmail.com>
2021-03-05 18:14:40 +0300

    set data from galleries class

--- app/src/main/java/com/example/galery/GalleryPreviewActivity.java
+++ app/src/main/java/com/example/galery/GalleryPreviewActivity.java
@@ -3,34 +3,64 @@ package com.example.galery;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.galery.adapter.PreviewAdapter;
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class GalleryPreviewActivity extends AppCompatActivity {
 
     private RecyclerView galleryImages;
     private PreviewAdapter previewAdapter;
+    private TextView galleryName;
+    private String newName;
+    private String jsonImages;
+    private ImageInfo[] images;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_galery);
-        initRecyclerView();
+
+        Bundle galleryData = getIntent().getExtras();
+        if (galleryData != null) {
+            newName = galleryData.getString("gallery");
+            jsonImages = galleryData.getString("images");
+
+        }
+
+        GsonBuilder builder = new GsonBuilder();
+        Gson gson = builder.create();
+
+        images = gson.fromJson(jsonImages, ImageInfo[].class);
+
+        galleryName = findViewById(R.id.gallery_name);
+        galleryName.setText(newName);
+        ByteArrayOutputStream out = new ByteArrayOutputStream();
+
+        initRecyclerView(images);
 
     }
 
-    public void initRecyclerView(){
+    public void initRecyclerView(ImageInfo[] images){
         galleryImages = findViewById(R.id.galery_list);
         galleryImages.setLayoutManager(new LinearLayoutManager(this));
 
         previewAdapter = new PreviewAdapter(this);
 
         galleryImages.setAdapter(previewAdapter);
-
+        previewAdapter.setItems(images);
     }
 
     public void onSelectGallery(View view) {

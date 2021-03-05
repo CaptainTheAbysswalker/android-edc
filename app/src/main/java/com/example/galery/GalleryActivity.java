a0af53226dd1f3512f5b01e0ae93f0c121729a69
CaptainTheAbysswalker <shuric03@gmail.com>
2021-03-05 18:14:57 +0300

    set data from galleries class

--- app/src/main/java/com/example/galery/GalleryActivity.java
+++ app/src/main/java/com/example/galery/GalleryActivity.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -14,14 +16,26 @@ import com.example.galery.adapter.GalleryAdapter;
 
 public class GalleryActivity extends AppCompatActivity {
 
-
+    private ImageInfo imageInfo;
+    private TextView artist;
+    private TextView description;
+    private ImageView image;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.fragment_image_description);
 
+        Bundle imageData = getIntent().getExtras();
+        imageInfo = (ImageInfo) imageData.getSerializable("image");
 
+        artist = findViewById(R.id.info_artist);
+        description = findViewById(R.id.info_description);
+        image = findViewById(R.id.image_full);
+
+        image.setImageResource(imageInfo.getSrc());
+        artist.setText(imageInfo.getArtist());
+        description.setText(imageInfo.getDescription());
     }
 
 }
